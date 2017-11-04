cf2598da6cf08f7ef39c9f1634c8fedc61b5da31
wangbiaoshuai <530800973@qq.com>
2017-11-04 22:16:20 +0800

    增加安装卸载脚本

--- client/bin/install.sh
+++ client/bin/install.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
 install_dir="/usr/local"
-DEAMON_DIR="${install_dir}/hot_backup_server"
-proc_name="hot_backup_server"
+DEAMON_DIR="${install_dir}/hot_backup_client"
+proc_name="hot_backup_client"
 DEAMON="${DEAMON_DIR}/${proc_name}"
+server_ip="$1"
+server_port=8101
 
 mkdir -p ${install_dir}
 cp -r ./bin ${DEAMON_DIR}
@@ -11,7 +13,7 @@ cp -r ./bin ${DEAMON_DIR}
 cd ${DEAMON_DIR} && chmod +x switch_ip.sh && cd -
 
 pgrep ${proc_name} | xargs -t -I{} kill -9 {}
-${DEAMON}
+${DEAMON} $server_ip $server_port
 
 pid=`pgrep ${proc_name}`
 if [ "${pid}" = "" ]
--- client/bin/uninstall.sh
+++ client/bin/uninstall.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 install_dir="/usr/local"
-DEAMON_DIR="${install_dir}/hot_backup_server"
-proc_name="hot_backup_server"
+DEAMON_DIR="${install_dir}/hot_backup_client"
+proc_name="hot_backup_client"
 
 pgrep ${proc_name} | xargs -t -I{} kill -9 {}
 
